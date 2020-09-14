692e581b87f49cdf2f363055d171220f80f6848a
spking <43746810+spking@users.noreply.github.com>
2020-09-14 16:04:40 +0000

    Add files via upload

---
+++ scripts/collision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public playermovement movement;
+    public GameManager gameManager;
+
+    void OnCollisionEnter(Collision collisionInfo){
+        if (collisionInfo.collider.tag == "Obsacle"){
+        movement.enabled = false;
+        FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+}
