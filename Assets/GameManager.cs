aafd95ad7bc30403fc6f797c3404887d2040d363
spking <43746810+spking@users.noreply.github.com>
2020-09-15 18:09:25 +0000

    Add files via upload

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gamehasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public void EndGame ()
+    {
+        if (gamehasEnded == false){
+            gamehasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart (){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/scripts/playermovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float Force = 2000f;
+
+    public float SideForce = 10000f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, Force * Time.deltaTime);
+        }
+        if (Input.GetKey("d")) 
+        {
+            rb.AddForce(SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a")) 
+        {
+            rb.AddForce(-SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s")) 
+        {
+            rb.AddForce( 0, 0, -Force * Time.deltaTime);
+        }
+        if (rb.position.y < -1f){
+            FindObjectOfType<GameManager>().EndGame(); 
+        }
+    }
+}
