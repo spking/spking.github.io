16f88dce23cc3486d051037b0021100fb176f669
spking <43746810+spking@users.noreply.github.com>
2020-10-14 17:34:33 +0000

    Add files via upload

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //fá x gildi músarinnar og snúa targetinu
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        transform.position = target.position - offset;
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed;
+    //public Rigidbody rigidbody;
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       controller = GetComponent<CharacterController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rigidbody.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        /*if(Input.GetButtonDown("Jump"));
+        {
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpForce, rigidbody.velocity.z);
+        }*/
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (controller.isGrounded)
+        {
+            moveDirection.y =0f;
+            if(Input.GetButtonDown("Jump"));
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
